45447009f0be7c0931f38c2a4f4d1af66b850d14
lakhdaad <adam.lakhdari@etu.univ-smb.fr>
2023-01-03 11:21:30 +0100

    fdsfds

--- SAE DEV/JeuxMonogame/Game1.cs
+++ SAE DEV/JeuxMonogame/Game1.cs
@@ -8,7 +8,7 @@ namespace JeuxMonogame
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        private int _vitessedebasejoueur^2;
+        private int _vitessedebasejoueur;
 
         public Game1()
         {
